48b184c8b11d587dc6ae273f732ea48b8cdf07b7
Saleh-Tarek <saleh.tarek001@gmail.com>
2023-09-02 13:24:03 +0300

    My First Commit

---
+++ _01_C_Course/C_Course_Labs/hello.c
@@ -0,0 +1,16 @@
+/*
+*	Hello C program	: 
+*	Function	: print text "Hello C"
+*	Date		: 01/09/2023
+*	Auther		: Tarek
+*/
+#include <stdio.h>
+
+
+int main(){
+
+	printf("Hello C\n");
+	
+
+	return(0); 
+}
